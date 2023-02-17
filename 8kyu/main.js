285dd04777e17789a6ae8688257f7797269e9cb2
Sacarima <caroline12alex@yahoo.com>
2023-02-17 09:46:09 +0100

    Multiplication of arrays, solution

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -13,3 +13,16 @@ function invert(array) {
 
 console.log(array([-1,-2,-3,-4,-5]))
 
+/*
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+*/
+
+function grow(x) {
+    return x.reduce((a, b)=> (a * b))
+}
+
+console.log(grow([1, 2, 3, 4] ))
