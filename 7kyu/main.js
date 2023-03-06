de65576bf650ca2ab085a8dd50876e11e902774e
Sacarima <caroline12alex@yahoo.com>
2023-03-06 13:12:24 +0100

    lowest value addition, arrays

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -50,3 +50,26 @@ function rowSumOddNumbers(n) {
 function rowSumOddNumbers(n) {
     return Math.pow(n, 3);
   }
+
+/*
+    Create a function that returns the sum of the two lowest positive numbers given an 
+    array of minimum 4 positive integers. No floats or non-positive integers will be 
+    passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+*/
+
+// Solution 1
+
+ function sumTwoSmallestNumbers(numbers) {  
+  const sortTheNumbers = numbers.sort((a, b) =>  a - b);
+  return sortTheNumbers[0] + sortTheNumbers[1];
+}
+
+// solution 2
+
+function sumTwoSmallestNumbers(numbers) {  
+  return numbers.sort((a,b)=>a>b).slice(0,2).reduce((acc,curr)=>acc+curr);
+};
