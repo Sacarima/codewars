b85a72134ad1f9aa9e7f52dc5ef78e07e8356b55
Sacarima <caroline12alex@yahoo.com>
2023-03-09 14:02:00 +0100

    solution to max and min numbers in the array

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -73,3 +73,33 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 function sumTwoSmallestNumbers(numbers) {  
   return numbers.sort((a,b)=>a>b).slice(0,2).reduce((acc,curr)=>acc+curr);
 };
+
+/*
+  Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples (Input --> Output)
+[1,2,3,4,5] --> [1,5]
+[2334454,5] --> [5,2334454]
+[1]         --> [1,1]
+Remarks
+All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+
+*/
+
+// solution 1
+
+function minMax(number){
+  return [Math.min(...number), Math.max(...number)]
+}
+
+// solution 2
+
+function minMax(number) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  return [min, max];
+}
