49d75cc269df87e19580e2857a73feb99f2c66a3
Sacarima <caroline12alex@yahoo.com>
2023-03-22 01:33:12 +0100

    solution to binary conversion

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -142,3 +142,25 @@ function squareDigits(num){
   }
   return Number(results.join(''));
 };
+
+/*
+  Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+*/
+
+//solution 1
+
+function addBinary(a,b) {
+  return (a + b).toString(2)
+}
+
+// solution 2
+
+const addBinary = (a, b) => Math.trunc(a + b).toString(2)
+
