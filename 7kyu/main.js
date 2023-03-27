b4c643607a536cc85e4859ba1f06c3f0c9dd23f8
Sacarima <caroline12alex@yahoo.com>
2023-03-27 19:54:58 +0200

    solution to sum of all minimum values in the array

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -262,6 +262,45 @@ function oodOrEven(array) {
     return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
   }
 
+  /*
+    Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+For Example:
+
+[ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+, [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+, [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+]
+So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+Note: You will always be given a non-empty list containing positive values.
+  */
+
+
+// solution 1
+
+function sumOfMinimums(arr) {
+  const sum = arr.map(x=> Math.min(...x)).reduce((a, b) => (a + b), 0)
+   
+   return sum;
+}
+
+// solution 2
+
+function sumOfMinimums(arr) {
+  return arr.reduce((p, c) => p + Math.min(...c), 0);
+}
+
+//solution 3
+
+function sumOfMinimums(arr) {
+  let total = 0;
   
+  for (let i = 0; i < arr.length; i++){
+   total += Math.min(...arr[i])
+  }
+  
+  return total;
+}
 
 
