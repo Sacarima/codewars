29c371af3979deee133f48c524c68c7c8840f446
Sacarima <caroline12alex@yahoo.com>
2023-03-29 18:51:32 +0200

    solution to filtering array

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -303,4 +303,27 @@ function sumOfMinimums(arr) {
   return total;
 }
 
+/*
+  JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+*/
+
+// solution 1
+
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(x=> {
+    return x % 2 == 0;
+  })
+}
+
+// solution 2
+
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(function(num){return !(num % 2) })  // 0 is falsy
+}
+
+
 
