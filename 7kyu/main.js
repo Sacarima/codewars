7d1ed204fe5189745f559fbfdf7554e67d2f3203
Sacarima <caroline12alex@yahoo.com>
2023-03-30 11:59:41 +0200

    solution to language skills

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -358,7 +358,29 @@ function removeSmallest(numbers) {
   return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
 }
 
-//solution 3
+/*
+  You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
+
+Note: the scores will always be unique (so no duplicate values)
+
+Examples
+{"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+{"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+{"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+
+*/
+
+//solution 1
+
+function myLanguages(results) {
+  let totalResult = [];
+  for(let i in results) {
+    if(results[i] >= 60) {
+      totalResult.push(i)
+    }
+  }
+  return totalResult.sort((a, b) => results[b] - results[a])
+}
 
 
 
