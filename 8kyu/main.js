e6c8292c202f10721b0584021d06ec1b408ba263
Sacarima <caroline12alex@yahoo.com>
2023-04-03 04:16:25 +0200

    chnage string to uppercase

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -177,3 +177,9 @@ function toNumber (str) {
 } 
 
 
+//Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase()
+}
