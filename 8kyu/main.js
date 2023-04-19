ded8195c664259423eef6d305fc9e4e113b3d0e6
Sacarima <caroline12alex@yahoo.com>
2023-04-19 04:45:51 +0200

    solution to remove first and last character in a string

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -183,3 +183,23 @@ function makeUpperCase(str) {
   // Code here
   return str.toUpperCase()
 }
+
+/*
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+*/
+
+//solution 1
+
+function removeChar(str){
+  
+  return str.slice(1, -1)
+
+};
+
+//solution 2
+
+function removeChar(str){
+  return str.substring(1, str.length-1);
+ };
+ 
