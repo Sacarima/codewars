9e2e66f92df8caaad5dc66207b04f258256ccea1
Sacarima <caroline12alex@yahoo.com>
2023-04-22 22:08:59 +0200

    solution to prototype styring to uppercase

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -252,3 +252,31 @@ var countSheep = function (num){
 const countSheep = function(num) {
     return [...Array(num).keys()].reduce((res, i) => res + (i + 1) + ' sheep...', '');
   }
+
+  /*
+Is the string uppercase?
+Task
+Create a method to see whether the string is ALL CAPS.
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+*/
+
+//Solution 1
+
+String.prototype.isUpperCase = function() {
+  // your code here
+   return this.toString() === this.toUpperCase()
+}
+
+//solution 2
+
+String.prototype.isUpperCase = function() {
+  return this.toUpperCase() == this;
+}
