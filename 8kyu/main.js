610fd3aa755dd0f3358a7349fb4cf46fcf300f33
Sacarima <caroline12alex@yahoo.com>
2023-05-02 19:20:38 +0200

    solution to fake binary

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -280,3 +280,57 @@ String.prototype.isUpperCase = function() {
 String.prototype.isUpperCase = function() {
   return this.toUpperCase() == this;
 }
+/*
+  Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+
+
+*/
+
+//solution 1
+
+function abbrevName(name){
+  return name.match(/\b([A-Z])/g).join('.')
+}
+
+/*
+Fake Binary
+  Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+*/
+
+//solution 1
+
+function fakeBin(x){
+  let sum = "";
+  for(let i = 0; i < x.length; i++) {
+    if(x[i] < 5) {
+      sum += 0;
+    }else {
+      sum += 1;
+    }
+  }
+  return sum
+}
+
+
+//solution 2
+
+function fakeBin(x) {
+  return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
+
+//solution 3
+
+function fakeBin(x) {
+  return x.replace(/\d/g, d => d < 5 ? 0 : 1);
+}
