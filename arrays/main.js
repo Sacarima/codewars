657a953aec454a4c17c01e106defbb86b0dda4d7
Sacarima <caroline12alex@yahoo.com>
2023-05-08 09:06:20 +0200

    solution to sorting inventors by the years they were born

--- arrays/main.js
+++ arrays/main.js
@@ -42,3 +42,11 @@ const order = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
     }, 0);
 
     console.log(totalYears);
+
+     // 5. Sort the inventors by years lived
+     const oldest = inventors.sort(function(a, b) {
+      const lastInventor = a.passed - a.year;
+      const nextInventor = b.passed - b.year;
+      return lastInventor > nextInventor ? -1 : 1;
+    });
+    console.table(oldest);
