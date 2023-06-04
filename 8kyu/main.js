ee355926770e54ceae5eb571ee8e31270ea6f3ba
Sacarima <caroline12alex@yahoo.com>
2023-06-04 21:09:04 +0200

    solution to count the number of monkeys

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -386,3 +386,35 @@ function getAverage(marks){
 function getAverage(marks){
   return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
 }
+
+/**
+    Count the Monkeys!
+
+    You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+For example(Input --> Output):
+
+10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ 1 --> [1]
+ */
+
+ //solution 1
+
+ function monkeyCount(n) {
+  // your code here
+    let sum = [];
+    for(let i = 1; i < n + 1; i++) {
+      sum.push(i)
+    }
+    return sum;
+    
+  }
+
+  //solution 2
+
+  function monkeyCount(n) {
+    return Array.from({length:n}, (_,i)=>i+1)
+  }
+  
