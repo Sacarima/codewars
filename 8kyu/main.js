10f3bb8f6f7c26a845d7d927dbd0fcc0dff4c3b1
Sacarima <caroline12alex@yahoo.com>
2023-06-07 08:20:04 +0200

    solution to Grasshopper - summmation

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -417,4 +417,25 @@ For example(Input --> Output):
   function monkeyCount(n) {
     return Array.from({length:n}, (_,i)=>i+1)
   }
-  
+/*
+    Grasshopper - Summation
+    Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example (Input -> Output):
+
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+*/  
+
+// Solution 1
+
+
+var summation = function (num) {
+  // Code here
+  let sum = 0;
+  for(let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
