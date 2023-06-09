a5ccfd54b009e0c9e07f40d150487353f2310136
Sacarima <caroline12alex@yahoo.com>
2023-06-09 12:41:56 +0200

    solution to Testing 1-2-3

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -418,7 +418,24 @@ const helloWorld = () => String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87,
 
 const helloWorldH = () => /Hello, World!/.source
 
+/*
+  Testing 1-2-3
 
+  Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
 
+Write a function which takes a list of strings and returns each line prepended by the correct number.
 
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
 
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3
+*/
+
+// solution 1
+
+var number=function(array){
+  //your awesome code here
+  return array.map((element, index) => `${index + 1}: ${element}`)
+}
