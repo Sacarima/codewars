c323d33c6f7a2a4540cd24eab02aab41da845f99
Sacarima <caroline12alex@yahoo.com>
2023-06-11 07:31:07 +0200

    solution 2 to Find the first non-consecutive number

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -494,3 +494,11 @@ function firstNonConsecutive (arr) {
    };
    return null;
 }
+
+// solution 2
+
+function firstNonConsecutive (arr) {
+  let result = arr.find((val, index) => val !== index + arr[0]);
+
+  return (Number.isInteger(result)) ? result : null;
+}
