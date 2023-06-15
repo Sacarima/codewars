769d6a045a94804f1c0a78cb1e3dc78b80580f56
Sacarima <caroline12alex@yahoo.com>
2023-06-15 20:08:45 +0200

    solution  to remove every second element

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -502,3 +502,24 @@ function firstNonConsecutive (arr) {
 
   return (Number.isInteger(result)) ? result : null;
 }
+
+/*
+  Removing Elements
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+
+*/
+
+//solution 1
+
+function removeEveryOther(arr){
+  let result = [];
+  for( let i = 0; i < arr.length; i+=2 ) {
+    result.push(arr[i])
+  }
+  return result;
+}
