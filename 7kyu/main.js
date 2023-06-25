3d5592d01e34ca91c16bf0f4345b2ded1850843f
Sacarima <caroline12alex@yahoo.com>
2023-06-25 19:37:21 +0200

    solution to vowel count

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -496,3 +496,26 @@ function twoOldestAges(ages){
   
   return [second, oldest];
 }
+
+/*
+VOWEL COUNT
+
+    Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+// SOLUTION 1
+
+function getCount(str) {
+  let vowelC = 0;
+  const vowel = ['a', 'e', 'i', 'o', 'u'];
+  for(let char of str) {
+    if(vowel.includes(char)) {
+      vowelC++;
+    }
+  }
+  return vowelC;
+}
