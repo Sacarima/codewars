0bdf89b3ff7571c511011c1914dee9cdc324d0b2
Sacarima <caroline12alex@yahoo.com>
2023-06-28 22:42:43 +0200

    solution 2  to maximum product

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -651,3 +651,13 @@ function adjacentElementsProduct(array) {
   }
   return maximumProduct
 }
+
+// Solution 2
+
+function adjacentElementsProduct(array) {
+  let newArr = []
+  for(i=0; i < array.length-1; i++){
+    newArr.push(array[i]*array[i+1])
+  }  
+  return Math.max(...newArr)
+}
