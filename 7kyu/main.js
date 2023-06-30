16bc2ba888a7ef70af8cbe89d02299baf5fa120c
Sacarima <caroline12alex@yahoo.com>
2023-06-30 04:53:43 +0200

    solution 4  to flatten and sort and array

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -758,3 +758,18 @@ function flattenAndSort(array) {
     .sort((a, b) => a - b)
     ;
 }
+
+// solution 4
+
+function flattenAndSort(array) {
+  var newArray = [];
+  for(var i =0;i<array.length;i++)
+  {
+    for(var j=0;j<array[i].length;j++)
+    {
+      newArray.push(array[i][j]);
+    }
+  }
+    
+  return newArray.sort((a,b) =>  a-b);
+}
