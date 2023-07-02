e0dd5c1f80e374cc95c87a94a25ad91396a6929d
Sacarima <caroline12alex@yahoo.com>
2023-07-02 03:34:02 +0200

    solution4 to find the capitals

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -810,3 +810,13 @@ const capitals3 = word => {
   const capsArr = word.match(/[A-Z]/g)
   return capsArr.map( x => { 
     return word.indexOf(x) }) }
+
+// solution 4
+
+ const capitals4 = function (word) {
+  var caps = [];
+  for(var i = 0; i < word.length; i++) {
+    if(word[i].toUpperCase() == word[i]) caps.push(i);
+  }
+  return caps;
+};
