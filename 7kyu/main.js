d9ce41b635bd37400f65944b0cd2922dedaaba8e
Sacarima <caroline12alex@yahoo.com>
2023-07-02 03:30:08 +0200

    solution to find the capitals

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -778,3 +778,19 @@ function flattenAndSort(array) {
 
 
 const flattenAndSort = $ => $.toString().split(',').filter(e => e).map(Number).sort((a,b)=>a-b)
+
+/*
+    FIND THE CAPITALS
+
+    Write a function that takes a single string (word) as argument. 
+    The function must return an ordered list containing the indexes of all capital letters in the string.
+*/
+
+
+// Solution 1
+
+var capitals = function (word) {
+  return word.split('')
+             .map(function(l, i) { if (l.toUpperCase() === l) return i; })
+             .filter(function(i) { return i != null })
+};
