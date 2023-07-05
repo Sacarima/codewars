d3c27e2252781f6f23deabe8c8d027f874f269c6
Sacarima <caroline12alex@yahoo.com>
2023-07-05 04:05:50 +0200

    solution 3 to are you playing banjo

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -697,4 +697,15 @@ function areYouPlayingBanjo(name) {
   }
 }
 
+// Solution 3
+
+
+function areYouPlayingBanjo(name) {
+ 
+  if (name[0] == 'R' || name[0] == 'r')
+    return name + " plays banjo";
+  else
+    return name + " does not play banjo";
+}
+
 
