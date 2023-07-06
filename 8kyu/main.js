e07db92cca0186a14ac8336e5da6665bd95e598c
Sacarima <caroline12alex@yahoo.com>
2023-07-06 22:04:21 +0200

    solution to All star Code challenge#18

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -767,3 +767,34 @@ function removeExclamationMarks(s) {
 const removeExclamationMarks = (s) => {
   return s.split('').filter((letter) => letter !== '!').join('')
 }
+
+/**
+ * 
+        All star Code challenge#18
+Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned.
+
+("Hello", "o")  ==>  1
+("Hello", "l")  ==>  2
+("", "z")       ==>  0
+str_count("Hello", 'o'); // returns 1
+str_count("Hello", 'l'); // returns 2
+str_count("", 'z'); // returns 0
+Notes
+The first argument can be an empty string
+In languages with no distinct character data type, the second argument will be a string of length 1
+
+ */
+
+// Solution 1
+
+function strCount(str, letter){  
+  let result = 0
+  for (let i = 0; i < str.length; i++) {
+    if(str[i] === letter) {
+      result++
+    }
+  }
+  return result
+}
