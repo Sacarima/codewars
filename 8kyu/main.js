8ec18889672f0b238f291bbcd1bc8905f8195848
Sacarima <caroline12alex@yahoo.com>
2023-07-09 04:42:02 +0200

    solution 3 to correct mistakes of the character recognition software

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -964,3 +964,15 @@ function correct(string){
 const correct = string => (
 	string.replace(/[501]/g, character => corrections[character])
 );
+
+// Solution 3
+
+function correct(s){
+	s = s.split('');
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '5') s[i] = 'S';
+    else if (s[i] === '0') s[i] = 'O';
+    else if (s[i] === '1') s[i] = 'I';
+    }
+  return s.join('');
+}
