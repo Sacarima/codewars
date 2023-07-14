35ffc4e431f774921ca87ca123d30e2a6dd4182e
Sacarima <caroline12alex@yahoo.com>
2023-07-14 13:24:58 +0200

    solution 3 to descending order

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -945,3 +945,10 @@ function descendingOrder(n){
 function descendingOrder(n){
   return parseInt(String(n).split('').sort().reverse().join(''))
 }
+
+
+// Solution 3
+
+function descendingOrder(n){
+  return +(n + '').split('').sort(function(a,b){ return b - a }).join('');
+}
