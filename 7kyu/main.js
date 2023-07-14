a6b75a3a6846654da64e2978ddd7f5e52efa771b
Sacarima <caroline12alex@yahoo.com>
2023-07-14 13:22:36 +0200

    solution to descending order

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -917,3 +917,24 @@ function noOdds( values ){
 // Solution 3
 
 var noOdds = values => values.filter(x => !(x % 2));
+
+
+
+/*
+   Descending order
+
+   Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+*/
+
+// Solution 1
+
+function descendingOrder(n){
+  return Number([...String(n)].sort(( a, b ) => b - a ).join(''))
+}
