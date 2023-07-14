459cd13f671612cd5622a7c206aa529158093ee7
Sacarima <caroline12alex@yahoo.com>
2023-07-14 11:07:44 +0200

    No oddities here

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -884,3 +884,23 @@ var gimme = function (triplet) {
   }
   return index;
 };
+
+
+/**
+    no oddities here
+    Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+
+
+ */
+
+// Solution 1
+
+
+function noOdds( values ){
+  return values.filter((a) => a % 2 === 0 )
+}
+
+
+
