e88f24714812497ab3629196993f4b094b59bf23
Sacarima <caroline12alex@yahoo.com>
2023-07-20 17:29:02 +0200

    solution to fizzbuzz

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -1162,3 +1162,42 @@ function comp(array1, array2) {
   array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
   return array1.map(v => v * v).every((v, i) => v == array2[i]);
 }
+
+
+/* FIZZBUZZ
+    Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+N will never be less than 1.
+
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+*/
+
+
+// Solution 1 
+
+// Return an array
+function fizzbuzz(n)
+{
+  const result = []
+  for(let i = 1; i <= n; i++) {
+    if(i % 3 === 0 && i % 5 === 0 ) {
+        result.push("FizzBuzz");
+    }else if(i % 3 === 0) {
+        result.push("Fizz");
+    }else if(i % 5 === 0) {
+        result.push("Buzz");
+    }else {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
