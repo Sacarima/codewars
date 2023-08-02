68dc3b28c88671bbe7550ce3d323c8a966e59abf
Sacarima <caroline12alex@yahoo.com>
2023-08-02 13:20:07 +0200

    solution to multiplication

--- 6kyu/main.js
+++ 6kyu/main.js
@@ -81,3 +81,28 @@ function createPhoneNumber(numbers){
 }
 
 console.log()
+
+
+/* MULTIPLICATION
+
+  Your task, is to create N×N multiplication table, of size provided in parameter.
+
+For example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+For the given example, the return value should be:
+
+[[1,2,3],[2,4,6],[3,6,9]]
+*/
+
+// SOLUTION 1
+
+const multiplicationTable = size =>
+  Array.from(
+    {length: size}, (_,i) => Array.from(
+    {length: size}, (_,j) => (i + 1) * (j + 1)
+  )
+    
+  )
