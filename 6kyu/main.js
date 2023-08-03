88c302ac58aed5887ec1ee0748be08d0146bc750
Sacarima <caroline12alex@yahoo.com>
2023-08-03 13:57:14 +0200

    solution to data reverse

--- 6kyu/main.js
+++ 6kyu/main.js
@@ -164,3 +164,33 @@ function solution(number){
 function solution(number){
   return number < 1 ? 0 : [...new Array(number).keys()].filter(n => n % 3 == 0 || n % 5 == 0).reduce((a, b) => a + b);
 }
+
+/* //  DATA REVERSE
+
+  A stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+*/
+
+
+// SOLUTION 
+
+const dataReverse = data =>
+   new Array(data.length/8)
+  .fill('')
+.map((_,i) => data
+     .slice(i*8, (i + 1)* 8))
+     .reverse()
+     .flat()
