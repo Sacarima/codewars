a25a7ea0339c08a18285dbeea4e434efebaa5cf7
Sacarima <caroline12alex@yahoo.com>
2023-08-06 20:46:04 +0200

    solution2 to take a 10 minute walk

--- 6kyu/main.js
+++ 6kyu/main.js
@@ -283,3 +283,23 @@ const isValidWalk = walk => {
 
 return walk.length === 10 ? (result.n === result.s && result.w === result. e) : false
 }
+
+
+// Solution 
+
+function isValidWalk(walk) {
+  var dx = 0
+  var dy = 0
+  var dt = walk.length
+  
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n': dy--; break
+      case 's': dy++; break
+      case 'w': dx--; break
+      case 'e': dx++; break
+    }
+  }
+  
+  return dt === 10 && dx === 0 && dy === 0
+}
