85767523b56fc247df124b755605a97d8c0c8b1a
Sacarima <caroline12alex@yahoo.com>
2023-08-08 10:56:56 +0200

    solution 2  to find the unique string

--- 5kyu/main.js
+++ 5kyu/main.js
@@ -15,4 +15,22 @@ let newArr = arr.map(a => { return [...new Set(a.toLowerCase())].sort().join('')
   for ( let i = 0; i < newArr.length; i++ ) {
     if ( newArr.indexOf(newArr[i]) === newArr.lastIndexOf(newArr[i]) ) return arr[i]
   }
+
+// SOLUTION 2
+
+function findUniq(arr) {
+    let [a,b,c] = arr.slice(0,3)
+    
+    if (!similar(a,b) && !similar(a,c)) return a
+    for (d of arr) if (!similar(a,d)) return d
+  }
   
+  function similar (x, y) {
+    x = new Set(x.toLowerCase())
+    y = new Set(y.toLowerCase())
+  
+    if (x.size !== y.size) return false
+    for (z of x) if (!y.has(z)) return false
+  
+    return true
+  }
