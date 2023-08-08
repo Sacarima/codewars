28cead8790224f56c391510590e7ba33ec801e52
Sacarima <caroline12alex@yahoo.com>
2023-08-08 10:58:05 +0200

    solution 3  to find the unique string

--- 5kyu/main.js
+++ 5kyu/main.js
@@ -34,3 +34,11 @@ function findUniq(arr) {
   
     return true
   }
+
+  // SOLUTION 3
+
+  const unique = (x, i, ar) => ar.indexOf(x) === ar.lastIndexOf(x);
+
+const getUniques = x => [...new Set([...x.toLowerCase()].sort())].join('');
+
+const findUniq = arr => arr[arr.map(getUniques).findIndex(unique)];
