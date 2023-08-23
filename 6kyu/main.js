328d0bd758550c8816c3c069b93e8be81e23cde4
Sacarima <caroline12alex@yahoo.com>
2023-08-23 22:26:00 +0200

    font change

--- 6kyu/main.js
+++ 6kyu/main.js
@@ -6,7 +6,6 @@ For the purposes of this kata, some tests may have multiple answers; any valid s
 The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
 
 Based on: http://oj.leetcode.com/problems/two-sum/
-
 twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
 */
 
