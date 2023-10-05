03bbacb8dcbf3c9a12a92279d1e99573039a9a0b
Sacarima <caroline12alex@yahoo.com>
2023-10-05 11:04:47 +0200

    solution 3  to removing duplicates fromthe array

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -1216,3 +1216,15 @@ function distinct(a) {
   return Array.from(new Set(a));
 }
 
+// solution 3
+
+function distinct(arr) {
+  let res = []; 
+  
+  for(let i = 0; i < arr.length; i++){
+    if(!res.includes(arr[i])){
+      res.push(arr[i]);
+    }
+}
+return res;
+}
