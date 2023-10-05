1030f3c004204e5d85eff4eed622a8340306f453
Sacarima <caroline12alex@yahoo.com>
2023-10-05 10:56:25 +0200

    solution to removing duplicates fromthe array

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -1188,3 +1188,24 @@ function addLength(str) {
   } 
   return result;  
 }
+
+// Remove duplicates from list
+
+/*
+  Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+*/
+
+
+// SOLUTION 1
+
+function distinct(a) {
+  return [... new Set(a)];
+}
