b759ff479cfb8d9dbe2dc159c514f957a27f058c
Sacarima <caroline12alex@yahoo.com>
2023-10-11 05:57:48 +0200

    solution 3  merging two sorted arrays into one

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -1298,3 +1298,10 @@ function mergeArrays(arr1, arr2) {
 function mergeArrays(arr1, arr2) {
   return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
 }
+
+
+// SOLUTION 3
+
+function mergeArrays(a, b) {
+  return [...new Set(a.concat(b))].sort((a,b)=>a-b)
+}
