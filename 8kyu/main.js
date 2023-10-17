53353837866999c855a1ab7532ffe2e5e87ee17b
Sacarima <caroline12alex@yahoo.com>
2023-10-17 05:16:09 +0200

    solution to stringy string

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -1305,3 +1305,34 @@ function mergeArrays(arr1, arr2) {
 function mergeArrays(a, b) {
   return [...new Set(a.concat(b))].sort((a,b)=>a-b)
 }
+
+/**
+  STRINGY STRINGS
+
+    write me a function stringy that takes a size and returns a string of alternating 1s and 0s.
+
+the string should start with a 1.
+
+a string with size 6 should return :'101010'.
+
+with size 4 should return : '1010'.
+
+with size 12 should return : '101010101010'.
+
+The size will always be positive and will only use whole numbers.
+ */
+
+// SOLUTION 1
+
+
+function stringy(size) {
+  let res = ''
+  for(let i = 0; i < size; i++) {
+    if(i % 2 === 0) {
+      res += 1
+    }else {
+      res += 0
+    }
+  }
+  return res
+}
