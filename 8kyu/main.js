c1dd8e8786d47ee2f415a391ba4b6fe4985d298e
Sacarima <caroline12alex@yahoo.com>
2024-03-31 23:37:07 +0200

    sum of differences

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -1438,3 +1438,52 @@ function logicalCalc(array, op){
   }
   return result
 }
+
+
+
+/**
+ * 
+ * Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+Example
+[2, 1, 10]  -->  9
+In descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+
+
+ */
+
+
+// Solution 1
+
+function sumOfDifferences(arr) {
+  // create a function with an array as an argument
+  // sortout the array in descending order, from highest to lowest
+  // create a variable call it sum store the comulative values
+  // loop through the array, iterate over the array from the first element ( i = 0)
+  // to the second to the last the element ( i = length of arr - 2)
+  // this is because the last element does not have subsequesnt element to form pair for 
+  // comparision
+  //calculate and the differences. in each iteration of the loop, the difference 
+  // between the current arr[i] and the next element arr[i + 1] is calculated
+    arr.sort((a, b) => b - a) // sort going up
+    let sum = 0;
+    for(let i = 0; i < arr.length - 1; i++ ) {
+      sum += arr[i] - arr[i + 1] // adding the differences between consecutive pairs
+    }
+    return sum
+  }
+
+
+  // Solution 2
+  function sumOfDifferences(arr) {
+    arr.sort(( a, b ) => b - a)
+    let sum = 0
+    for ( let i = 0; i < arr.length -1; i++ ) {
+      sum += arr[i] - arr[ i + 1]
+    }
+    return sum
+  }
