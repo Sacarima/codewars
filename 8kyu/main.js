7b73f2639807bca761ab2e10a9b95831f1e343a3
Sacarima <caroline12alex@yahoo.com>
2024-04-13 22:40:50 +0200

    solution to string abbreviation, solution 2

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -1569,3 +1569,24 @@ patrick feeney => P.F
 function abbrevName(name){
   return name.match(/\b(\w)/g).join('.').toUpperCase()
 }
+
+
+/*
+    Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+/ */
+
+// Solution 1
+
+function abbrevName(name){
+
+  return name.match(/\b(\w)/g).join('.').toUpperCase()
+
+}
