f65dfd1a2ea8b0df6b95fdec3df480b8cf158d81
Sacarima <caroline12alex@yahoo.com>
2024-04-17 23:34:12 +0200

    solution to well of ideas

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -1641,3 +1641,25 @@ Return true if the array contains the value, false if not.
     function check(a,x){
       return a.includes(x);
     }
+
+    /**
+     For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'.
+ If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. 
+ If there are no good ideas, as is often the case, return 'Fail!'.
+     */
+
+    // Solution 1
+
+    function well(x){
+      let countStrings = x.filter(item => item === 'good').length
+      
+      if( countStrings === 0 ) {
+        return 'Fail!'
+      }else if( countStrings  <= 2 ) {
+        return 'Publish!'
+      }else {
+        return 'I smell a series!'
+      }
+    }
